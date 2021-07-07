83a65e957309d21817e4cced55ffe09ad2de8472
RazanGurung <razangurung2147@gmail.com>
2021-07-07 21:02:17 +0545

    connected to database

--- src/component/Registration/Login.js
+++ src/component/Registration/Login.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './registration.css';
 import {GoogleLoginButton} from "react-social-login-buttons";
 import {Link} from "react-router-dom";
+import axios from 'axios';
+import {useHistory} from "react-router-dom";
 
 function Login() {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    let history = useHistory();
+
+    const loginUser=()=>{
+        const data = {
+            email:email,
+            password:password
+        }
+        axios.post("http://localhost:8000/user/login",data).then(response=>{
+            history.push("/")
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
     return (
         <div className="form-container">
             <div className="form">
                 <h1>Login</h1>
                 <p>Connect with us to use our Services</p>
                 <label>Email Address</label>
-                <input type="email" name="Email"  placeholder="Email" required="" />
+                <input type="email" name="Email"  placeholder="Email" required="" value={email} onChange={(e)=>setEmail(e.target.value)} />
                 <label>Password</label>
-                <input type="password" name="Password" placeholder="Password" required="" />
+                <input type="password" name="Password" placeholder="Password" required="" value={password} onChange={(e)=>setPassword(e.target.value)} />
                 <div className="checkbox">
                     <input type="checkbox" />
                     <label>Remember Me</label> 
                 </div>   
-                <button>login</button>
+                <button onClick={loginUser}>login</button>
                 <div className="social-account">Or Continue with your social account</div>
                 <GoogleLoginButton className="social-login" />
                 <div className="forget">
--- src/component/Registration/Register.js
+++ src/component/Registration/Register.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
+import {useHistory} from "react-router-dom";
 
 function Register() {
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+
+    let history = useHistory();
+    const sendInfo =()=>{
+        const data = {
+            firstname:firstname,
+            lastname:lastname,
+            email:email,
+            address:address,
+            phone:phone,
+            password:password
+        }
+        axios.post("http://localhost:8000/user/register",data).then(response=>{
+            alert("Registration Successful")
+            history.push("/sign-in")
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
+
     return (
         <div className="form-container">
             <div className="form-register">
@@ -9,30 +36,30 @@ function Register() {
                 <div className="sub-form">
                    <div className="option">
                         <label>First Name</label>
-                        <input type="text" name="firstname"  placeholder="First Name" required="" />
+                        <input type="text" name="firstname"  placeholder="First Name" required="" value={firstname} onChange={(e)=> setFirstName(e.target.value)}/>
                    </div>
                    <div className="option">
                         <label>Last Name</label>
-                        <input type="text" name="lastname"  placeholder="Last Name" required="" />
+                        <input type="text" name="lastname"  placeholder="Last Name" required="" value={lastname} onChange={(e)=> setLastName(e.target.value)}/>
                    </div>
                 </div>
                 <label>Email Address</label>
-                <input type="email" name="Email"  placeholder="Email" required="" />
+                <input type="email" name="Email"  placeholder="Email" required="" value={email} onChange={(e)=> setEmail(e.target.value)}/>
                 <div className="sub-form">
                    <div className="option">
                         <label>Address</label>
-                        <input type="text" name="address"  placeholder="Address" required="" />
+                        <input type="text" name="address"  placeholder="Address" required="" value={address} onChange={(e)=> setAddress(e.target.value)}/>
                    </div>
                    <div className="option">
                         <label>Phone No</label>
-                        <input type="tel" name="phone"  placeholder="Phone No" required="" />
+                        <input type="tel" name="phone"  placeholder="Phone No" required="" value={phone} onChange={(e)=> setPhone(e.target.value)}/>
                    </div>
                 </div>
                 <label>Password</label>
-                <input type="password" name="Password" placeholder="Password" required="" />
+                <input type="password" name="Password" placeholder="Password" required="" value={password} onChange={(e)=> setPassword(e.target.value)}/>
                 <label>Confirm Password</label>
                 <input type="password" name="Password" placeholder="Password" required="" />
-                <button>login</button>
+                <button onClick={sendInfo}>Register</button>
             </div>
         </div>
     )
